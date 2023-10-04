7b722c81ada57705b4d92417084d7378d714ae9f
Hieunv7801 <hieunv0708@gmail.com>
2023-10-04 09:03:39 +0700

    hieu

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">KHUYET TAT CSS </span>
+            Hi Everyone, I am <span className="purple">KHUYET TAT CSS 123 </span>
             from <span className="purple"> FPT SOFTWARE, DA NANG.</span>
             <br />I am a Fresher studying Predev09
             <br />
